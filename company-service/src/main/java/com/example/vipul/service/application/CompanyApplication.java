438570a2340a1140f659d9780612720323cdbb80
Vipul Tak <vipul.tak@BLRETV-C02VW2DTG.local>
2021-02-28 02:28:24 +0530

    swagger fix

--- company-service/src/main/java/com/example/vipul/service/application/CompanyApplication.java
+++ company-service/src/main/java/com/example/vipul/service/application/CompanyApplication.java
@@ -4,12 +4,25 @@ import com.example.vipul.service.web.FirstResource;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.federecio.dropwizard.swagger.SwaggerBundle;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 
 /**
  * @author VipulTak - 21/11/20
  */
 public class CompanyApplication extends Application<CompanyConfiguration> {
+
+    @Override
+    public void initialize(Bootstrap<CompanyConfiguration> bootstrap) {
+        bootstrap.addBundle(new SwaggerBundle<CompanyConfiguration>() {
+            @Override
+            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(CompanyConfiguration configuration) {
+                return configuration.getSwaggerBundleConfiguration();
+            }
+        });
+    }
 
     public void run(CompanyConfiguration companyConfiguration, Environment environment) throws Exception {
         Injector injector = Guice.createInjector(new CompanyModule());
